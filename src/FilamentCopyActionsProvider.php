01b4770d9f0e4d981afaa4ff2e4fbdfd84f486ca
Chee Hau <cheehau.lee11@gmail.com>
2023-12-14 12:00:16 +0800

    Update FilamentCopyActionsProvider.php

--- src/FilamentCopyActionsProvider.php
+++ src/FilamentCopyActionsProvider.php
@@ -5,15 +5,17 @@ namespace Webbingbrasil\FilamentCopyActions;
 use Filament\Facades\Filament;
 use Illuminate\Support\HtmlString;
 use Webbingbrasil\FilamentCopyActions\Forms\Actions\CopyAction;
-use Filament\PluginServiceProvider;
 use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class FilamentCopyActionsProvider extends PluginServiceProvider
+class FilamentCopyActionsProvider extends PackageServiceProvider
 {
+    public static string $name = 'filament-copyactions';
+
     public function configurePackage(Package $package): void
     {
         $package
-            ->name('filament-copyactions')
+            ->name(static::$name)
             ->hasViews();
     }
 
